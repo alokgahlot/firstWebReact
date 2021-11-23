4fa562b6ea7300720651ad7d86d158652b0d68ce
alokgahlot <alokgahlot99@gmail.com>
2021-11-23 12:51:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Settingd</li>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
